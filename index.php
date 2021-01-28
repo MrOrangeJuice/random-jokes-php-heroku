f1d6e579d269977a296cc2f72051de6e8b2a3950
Alex Parrotto <45072894+MrOrangeJuice@users.noreply.github.com>
2021-01-27 23:18:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,22 +16,22 @@
 	// this is an indexed array of associative arrays
 	// the associative arrays are jokes -  they have an 'q' key and an 'a' key
 	$jokes = [
-		["q"=>"What do you call a very small valentine?","a"=>"A valen-tiny!"],
-		["q"=>"What did the dog say when he rubbed his tail on the sandpaper?","a"=>"Ruff, Ruff!"],
-		["q"=>"Why don't sharks like to eat clowns?","a"=>"Because they taste funny!"],
-		["q"=>"What did the boy cat say to the girl cat?","a"=>"You're Purr-fect!"],
-		["q"=>"What is a frog's favorite outdoor sport?","a"=>"Fly Fishing!"],
-		["q"=>"I hate jokes about German sausages.","a"=>"Theyre the wurst."],
-		["q"=>"Did you hear about the cheese factory that exploded in France?","a"=>"There was nothing left but de Brie."],
-		["q"=>"Our wedding was so beautiful ","a"=>"Even the cake was in tiers."],
-		["q"=>"Is this pool safe for diving?","a"=>"It deep ends."],
-		["q"=>"Dad, can you put my shoes on?","a"=>"I dont think theyll fit me."],
-		["q"=>"Can February March?","a"=>"No, but April May"],
-		["q"=>"What lies at the bottom of the ocean and twitches?","a"=>"A nervous wreck."],
-		["q"=>"Im reading a book on the history of glue.","a"=>"I just cant seem to put it down."],
-		["q"=>"Dad, can you put the cat out?","a"=>"I didnt know it was on fire."],
-		["q"=>"What did the ocean say to the sailboat?","a"=>"Nothing, it just waved."],
-		["q"=>"What do you get when you cross a snowman with a vampire?","a"=>"Frostbite"]
+		["Q"=>"What do you call a very small valentine?","A"=>"A valen-tiny!"],
+		["Q"=>"What did the dog say when he rubbed his tail on the sandpaper?","A"=>"Ruff, Ruff!"],
+		["Q"=>"Why don't sharks like to eat clowns?","A"=>"Because they taste funny!"],
+		["Q"=>"What did the boy cat say to the girl cat?","A"=>"You're Purr-fect!"],
+		["Q"=>"What is a frog's favorite outdoor sport?","A"=>"Fly Fishing!"],
+		["Q"=>"I hate jokes about German sausages.","A"=>"Theyre the wurst."],
+		["Q"=>"Did you hear about the cheese factory that exploded in France?","A"=>"There was nothing left but de Brie."],
+		["Q"=>"Our wedding was so beautiful ","A"=>"Even the cake was in tiers."],
+		["Q"=>"Is this pool safe for diving?","A"=>"It deep ends."],
+		["Q"=>"Dad, can you put my shoes on?","A"=>"I dont think theyll fit me."],
+		["Q"=>"Can February March?","A"=>"No, but April May"],
+		["Q"=>"What lies at the bottom of the ocean and twitches?","A"=>"A nervous wreck."],
+		["Q"=>"Im reading a book on the history of glue.","A"=>"I just cant seem to put it down."],
+		["Q"=>"Dad, can you put the cat out?","A"=>"I didnt know it was on fire."],
+		["Q"=>"What did the ocean say to the sailboat?","A"=>"Nothing, it just waved."],
+		["Q"=>"What do you get when you cross a snowman with a vampire?","A"=>"Frostbite"]
 	];
 	
 	
